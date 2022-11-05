1e4f3cf6ca7530944109634ea9a303ce293ba76d
nuobasic <nuobasic@naver.com>
2022-11-06 02:43:32 +0900

    add: signUp controller 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/user-create.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signUp')
+  async signUp(createUsertDto: CreateUserDto) {
+    return await this.usersService.signUp(createUsertDto);
+  }
+}
