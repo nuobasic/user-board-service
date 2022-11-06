717089d8cd5ed018cff3e334bb01bbe5e08bfdd8
nuobasic <nuobasic@naver.com>
2022-11-06 15:59:04 +0900

    add: userservice 주석 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,11 +7,23 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  /**
+   * 유저 회원가입 서비스입니다.
+   * @url POST 'api/users/signup'
+   * @Body body 회원가입에 필요한 정보 { 이메일, 비밀번호,  }
+   * @returns 유저 회원가입
+   */
   @Post('signup')
   async signUp(@Body() createUsertDto: CreateUserDto) {
     return await this.usersService.signUp(createUsertDto);
   }
 
+  /**
+   * 유저 회원가입 서비스입니다.
+   * @url POST 'api/users/login'
+   * @Body 로그인의 필요한 정보 { 이메일, 비밀번호,  }
+   * @returns 유저 로그인
+   */
   @Post('login')
   async logIn(@Body() loginUserDto: LogInUserDto) {
     return await this.usersService.logIn(loginUserDto);
