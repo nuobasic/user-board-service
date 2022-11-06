56913b5cc2ae850e1096b1e97f14184a1383dff2
nuobasic <nuobasic@naver.com>
2022-11-06 13:42:30 +0900

    add: jwt strtegy

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,20 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entitiy/Users';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([Users]),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
